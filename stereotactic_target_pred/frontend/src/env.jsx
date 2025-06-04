7b2f87a04b9670ab8ef759b91fed401c9f2a5dfe
Mackenzie Snyder <masnyder2@hotmail.com>
2025-06-04 15:35:37 -0400

    Change dependencies from dev to deployment links

--- stereotactic_target_pred/frontend/src/env.jsx
+++ stereotactic_target_pred/frontend/src/env.jsx
@@ -1,9 +1,9 @@
 // For local development
 
-export const API_URL = "http://localhost:5000/";
+// export const API_URL = "http://localhost:5000/";
 
 // For server
 
-// export const API_URL = "https://validator.afids.io";
+export const API_URL = "https://validator.afids.io";
 
 // export const API_URL = "https://afids-stereotaxy-predict.onrender.com";
